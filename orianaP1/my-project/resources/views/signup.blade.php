d1d0e481538066793b46f3a49da090efda847963
cf20orianarojas <rojasoriana260217@gmail.com>
2023-11-19 22:20:50 +0100

    Pràctica1 Laravel: SIGNIN + SIGNUP

---
+++ orianaP1/my-project/resources/views/signup.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sessión</title>
+</head>
+<body>
+    <h1> {{ $titolLog }} </h1> <!-- Titol de la view amb els parametres de l'url definits al controlador -->
+    <form action="usuari.php" method="post">
+        <div>
+            <label for="email">Email</label>
+            <input type="text" name="email">
+        </div>
+        <div>    
+            <label for="password">Contrassenya</label>
+            <input type="password" name="password">
+        </div>
+        <div>
+            <input type="checkbox">
+            <label> Remember me </label>
+        </div><br>
+        <div>
+            <button type="submit" class="btn btn-primary" name="submit"> Enviar </button><br>
+        </div>
+        <div>
+            <a href="signin.blade.php">Crear d'usuari nou</a>
+        </div>
+    </form>
+</body>
+</html>
